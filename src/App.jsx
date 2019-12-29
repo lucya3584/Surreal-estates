faebd41eb6ace357a3c7d636af87324e41fea402
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-29 23:30:29 +0000

    fixing setup issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.App.css';
 import NavBar from './components/NavBar';
 import './App.css';
-import { tsConstructorType } from '@babel/types';
 
-function App extends Component {
-  constructor(props) {
-    super(props);
-
+class App extends Component {
+  render() {
+    return <NavBar />
   }
-
-  <div>
-
-
-    <p>
-      hello world.
-        </p>
-  </div>
-  );
 }
+
+
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './components/NavBar.css';
+import logo from './logo.png';
 
 
-function NavBar() {
-  return (
-    <div className="NavBar">
-      <img className="logo" src={logo} alt="SE Logo" />
-    </div>
-  );
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="NavBar">
+        <img className="logo" src={logo} alt="SE Logo" />
+        <ul className="nav">
+          <li className="item">View Properties</li>
+          <li className="item">Add a Property</li>
+        </ul>
+      </div>
+    );
+  }
 }
 export default NavBar
