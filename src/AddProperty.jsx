45476d22a9b426c30b635cafeaac2cad5836513b
lucya3584 <lucya3584@yahoo.co.uk>
2020-01-01 17:14:49 +0000

    added handles and returns

--- src/AddProperty.jsx
+++ src/AddProperty.jsx
@@ -34,48 +34,47 @@ class AddProperty extends Component {
 
   render() {
     return <div className="AddProperty">
+      <form className="addForm" onSubmit={this.handleAddProperty}>
 
-    </div>
+        <div className="input">Listing title:</div>
+
+        <input className="input" type="text" name="title" onChange={this.handleFieldChange}></input>
+
+        <div className="input">Listing type:</div>
+
+        <select className="input" name="type" onChange={this.handleFieldChange}>
+          <option value="Flat">Flat</option>
+          <option value="Detached">Detached</option>
+          <option value="Semi-Detached">Semi-Detached</option>
+          <option value="Terraced">Terraced</option>
+          <option value="End of Terrace">End of Terrace</option>
+          <option value="Cottage">Cottage</option>
+          <option value="Bungalow">Bungalow</option>
+        </select>
+
+        <div className="input">City:</div>
+
+        <select className="input" name="city" onChange={this.handleFieldChange}>
+          <option value="Manchester">Manchester</option>
+          <option value="Leeds">Leeds</option>
+          <option value="Liverpool">Liverpool</option>
+        </select>
+
+        <div className="input">No. of bedrooms:</div><input className="input" type="text" name="bedrooms" onChange={this.handleFieldChange}></input>
+
+        <div className="input">No. of bathrooms:</div><input className="input" type="text" name="bathrooms" onChange={this.handleFieldChange}></input>
+
+        <div className="input">Price:</div><input className="input" type="text" name="price" onChange={this.handleFieldChange}></input>
+
+        <div className="input">Contact email:</div><input className="input" type="text" name="email" onChange={this.handleFieldChange}></input>
+
+        <button className="input" type="submit">Add</button>
+
+      </form>
+
+    </div >
   }
-
-
-  return<div className = "Add Property">
-    <form className="addForm" onSubmit={this.handleAddProperty}>
-  <div className="input">Listing title:</div>
-  <input className="input" type="text" name="title" onChange={this.handleFieldChange}></input>
-
-  <div className="input">Lising type:</div>
-  <select className="input" name="type" onChange={this.handleFieldChange}>
-    <option value="Flat">Flat</option>
-    <option value="Detached">Detached</option>
-    <option value="Semi-Detached">Semi-Detached</option>
-    <option value="Terraced">Terraced</option>
-    <option value="End of Terrace">End of Terrace</option>
-    <option value="Cottage">Cottage</option>
-    <option value="Bungalow">Bungalow</option>
-  </select>
-  <div className="input">City:</div>
-  <select className="input" name="city" onChange={this.handleFieldChange}>
-    <option value="Manchester">Manchester</option>
-    <option value="Leeds">Leeds</option>
-    <option value="Liverpool">Liverpool</option>
-
-  </select>
-
-  <div className="input">No. of bedrooms:</div><input className="input" type="text" name="bedrooms" onChange={this.handleFieldChange}></input>
-
-  <div className="input">No. of bathrooms:</div><input className="input" type="text" name="bathrooms" onChange={this.handleFieldChange}></input>
-
-  <div className="input">Price:</div><input className="input" type="text" name="price" onChange={this.handleFieldChange}></input>
-
-  <div className="input">Contact email:</div><input className="input" type="text" name="email" onChange={this.handleFieldChange}></input>
-
-  <button className="input" type="submit">Add</button>
-</form>
-</div >
-
-  }
-
 };
+
 
 export default AddProperty
